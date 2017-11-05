b2c4bac3c239abf2520a32288e3b3a099a17454c
Bertrand Chardon <bertrand.chardon@gmail.com>
2017-11-05 14:23:44 +0100

    Added template for / route

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package images
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func handler(response http.ResponseWriter, request *http.Request) {
+type ip struct {
+	address string
+}
+
+func imageHandler(response http.ResponseWriter, request *http.Request) {
 	fileName := request.URL.Path[len("/images/"):]
 	fmt.Fprintf(response, "%s", fileName)
 }
 
+func rootHandler(response http.ResponseWriter, request *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(response, ip{address: request.Header.Get("X-Forwarded-For")})
+}
+
 func init() {
-	http.HandleFunc("/images/", handler)
+	http.HandleFunc("/images/", imageHandler)
+	http.HandleFunc("/", rootHandler)
 	http.ListenAndServe(":8080", nil)
 }
